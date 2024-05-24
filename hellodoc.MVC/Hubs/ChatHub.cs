80c01f3c452abf03e44023159bc47b03166b57ee
Taksh@Tatvasoft <l710340@gmail.com>
2024-05-24 18:35:56 +0530

    push notifications

--- hellodoc.MVC/Hubs/ChatHub.cs
+++ hellodoc.MVC/Hubs/ChatHub.cs
@@ -1,5 +1,6 @@
 ﻿using hellodoc.DAL.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace SignalRChat.Hubs
@@ -39,6 +40,43 @@ namespace SignalRChat.Hubs
 
             // Send the message to the sender as well
             await Clients.Client(senderConnectionId).SendAsync("ReceiveMessage", "You", message);
+
+            // Send Notification
+            var NotiConnectionid = _context.UserConnections.Where(x => x.UserId == receiverId && x.RequestId == "0").Select(x => x.ConnectionId).FirstOrDefault();
+
+            await SendPushNotification(NotiConnectionid, message);
+        }
+
+        #endregion
+
+
+        #region Send Notification
+
+        public async Task SendPushNotification(string receiverConnectionId, string message)
+        {
+            string? aspId = _httpContextAccessor.HttpContext!.Session.GetString("aspNetUserId");
+            //var senderName = _context.Aspnetusers.FirstOrDefault(x => x.Id == aspId).Username;
+            var anu = _context.Aspnetusers.Include(x => x.Users).Include(x => x.Admins).Include(x => x.PhysicianAspnetusers).FirstOrDefault(x => x.Id == aspId);
+            if (anu.Users != null) 
+            {
+                var senderName = anu.Users.FirstOrDefault().Firstname + " " + anu.Users.FirstOrDefault().Lastname;
+                await Clients.Client(receiverConnectionId).SendAsync("ReceivePushNotification", message, senderName);
+            }
+            else if (anu.Admins != null)
+            {
+                var senderName = anu.Admins.FirstOrDefault().Firstname + " " + anu.Admins.FirstOrDefault().Lastname;
+                await Clients.Client(receiverConnectionId).SendAsync("ReceivePushNotification", message, senderName);
+            }
+            else if (anu.PhysicianAspnetusers != null)
+            {
+                var senderName = anu.PhysicianAspnetusers.FirstOrDefault().Firstname + " " + anu.PhysicianAspnetusers.FirstOrDefault().Lastname;
+                await Clients.Client(receiverConnectionId).SendAsync("ReceivePushNotification", message, senderName);
+            }
+            else
+            {
+                var senderName = anu.Username;
+                await Clients.Client(receiverConnectionId).SendAsync("ReceivePushNotification", message, senderName);
+            }
         }
 
         #endregion
@@ -54,7 +92,10 @@ namespace SignalRChat.Hubs
 
             if (!aspnetID.IsNullOrEmpty() && !Requestid.IsNullOrEmpty())
             {
-                UserConnection connectedUSerId = _context.UserConnections.Where(x => x.UserId == aspnetID && x.RequestId==Requestid).FirstOrDefault();
+                UserConnection connectedUSerId = _context.UserConnections.Where(x => x.UserId == aspnetID && x.RequestId == Requestid).FirstOrDefault();
+
+                UserConnection connectedUSerId2 = _context.UserConnections.Where(x => x.UserId == aspnetID && x.RequestId == "0").FirstOrDefault();
+
                 if (connectedUSerId == null)
                 {
                     UserConnection userConnection = new UserConnection();
@@ -67,6 +108,11 @@ namespace SignalRChat.Hubs
                 else
                 {
                     connectedUSerId.ConnectionId = Context.ConnectionId;
+
+                    if (connectedUSerId2 != null)
+                    {
+                        connectedUSerId2.ConnectionId = Context.ConnectionId;
+                    }
                     _context.SaveChanges();
                 }
             }
---
+++ hellodoc.MVC/wwwroot/js/service-worker.js
@@ -0,0 +1,15 @@
+﻿// service-worker.js
+
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        // Additional options can be added here
+    });
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    // Add logic to handle notification clicks, such as opening a chat window
+    clients.openWindow('/chatHub'); // Example: Open chat window
+});
